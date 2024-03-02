172ef78f31fcc180bf3f13d5efa106a8d0881ee8
Baptiste-Rajon <baptiste.rajon@sanofi.com>
2024-03-02 10:12:25 -0600

    Update V1.0.3

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ setup(
     version='1.0.2',
     author='Bapth',
     description='Check if libraries in a requirements file are up to date. Thsis script can update the file.',
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/bapth/check_library',
+    url='https://github.com/Bapth-R/check_library.git',
     py_modules=['check_library'],
+    license = 'Apache License, Version 2.0',
     install_requires=[
         'requests',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache 2',
         'Operating System :: OS Independent',
     ],
     entry_points={
